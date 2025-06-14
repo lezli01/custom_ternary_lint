f6ed3431f94bb6ff4427b4ac923be4f3c9a992ca
lezli01 <lezli01@gmail.com>
2025-06-14 11:08:53 +0200

    first implementation

--- example/custom_ternary_lint_example.dart
+++ example/custom_ternary_lint_example.dart
@@ -1,6 +1,1 @@
-import 'package:custom_ternary_lint/custom_ternary_lint.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- lib/custom_ternary_lint.dart
+++ lib/custom_ternary_lint.dart
@@ -1,8 +1,35 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-export 'src/custom_ternary_lint_base.dart';
+PluginBase createPlugin() => _TernaryPlugin();
 
-// TODO: Export any libraries intended for clients of this package.
+class _TernaryPlugin extends PluginBase {
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) => [
+        NoTernaryOperatorRule(),
+      ];
+}
+
+class NoTernaryOperatorRule extends DartLintRule {
+  NoTernaryOperatorRule()
+      : super(
+          code: const LintCode(
+            name: 'no_ternary_operator',
+            problemMessage: 'Avoid using ternary operators (?:).',
+            correctionMessage:
+                'Use if/else instead, or ConditionBuilder or some other approach.',
+          ),
+        );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addConditionalExpression((ConditionalExpression node) {
+      reporter.atNode(node, code);
+    });
+  }
+}
